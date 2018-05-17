7c5598bb4893a721ddb6cb4bb7162945cf81a0f8
electron101 <sabashlive@gmail.com>
2018-05-17 16:51:43 +0300

    Добавил старый файл в котором пытался переписать оригинал nl

--- my_nl.c
+++ my_nl.c
@@ -1,22 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+
 
 
 /* https://www.ibm.com/developerworks/ru/library/au-unix-getopt/index.html */
+
+  /* -i, --line-increment=ЧИСЛО       шаг увеличения номеров строк */
+  /* -s, --number-separator=СТРОКА    добавлять СТРОКУ после номера */
+  /* -v, --starting-line-number=ЧИСЛО первый номер строки для каждой логической */
+  /*                                  страницы */
+  /* -w, --number-width=ЧИСЛО         использовать заданное ЧИСЛО столбцов для */
+  /*                                  номеров строк */
+
 /* Файлы, чьи имена были дополнительно указаны в команде, 
  * будут использоваться в качестве входных файлов. 
  */
 
 
+/* Сообщение с информацией об опциях 
+ */
+void usage(char *progname) 
+{
+	printf("\nИспользование:\n\n");
+	printf("%s [ключ] \n\n", progname);
+	printf("Ключи:\n");
+	printf(" -k \t ключ шифровки/дешифровки\n");
+	printf(" -d \t ключ включения режима дешифровки\n");
+	printf(" -f \t файл для чтения сообщения\n");
+	printf(" -o \t файл для записи сообщения\n");
+	printf(" -e \t примеры использования программы\n");
+	printf(" -h \t вывод этого сообщения\n\n");
+	exit( EXIT_FAILURE );
+}
+
+
 /* Глобальная структура с аргументами 
  */
 struct global_args_t {
-	char		*key_string;	/* -k ключ */
-	int		decryption;	/* -d ключ */
-	const char	*in_filename;	/* -o ключ */
-	FILE		*in_file;
-	const char	*out_filename;	/* -f ключ */
-	FILE		*out_file;
-	char		*msg;		/* сообщение шифровки/дешифровки */
+	int	line_increment;		/* -i ключ */
+	char	*separator;		/* -s ключ */
+	int	start_number;		/* -v ключ */
+	int	number_width;		/* -w ключ */
+	char	**inputFiles;		/* входные файлы */
+	int	numInputFiles;		/* число входных файлов */
 } global_args;
+
+static const char *opt_string = "i:s:v:w:?";	/* строка с опциями getopt() */
+
+static const struct option longOpts[] = {
+	{ "no-index", no_argument, NULL, 'I' },
+	{ "language", required_argument, NULL, 'l' },
+	{ "output", required_argument, NULL, 'o' },
+	{ "verbose", no_argument, NULL, 'v' },
+	{ "randomize", no_argument, NULL, 0 },
+	{ "help", no_argument, NULL, 'h' },
+	{ NULL, no_argument, NULL, 0 }
+};
+
+void validate_args(int argc, char *argv[])
+{
+	/* Инизиализация структуры.
+	 * По умолчанию используются -v1 -i1 -sTAB -w6 
+	 */
+	global_args.line_increment = 1;		/* -i ключ */
+	global_args.separator      = "TAB";	/* -s ключ */
+	global_args.start_number   = 1;		/* -v ключ */
+	global_args.number_width   = 6;		/* -f ключ */
+	global_args.inputFiles     = NULL;	/* входные файлы */
+	global_args.numInputFiles  = 0;		/* число входных файлов */
+
+	
+	int opt = 0;
+	opterr	= 0;		/* отключим вывод ошибок в getopt()*/
+	int longIndex = 0;
+	
+	/* Обработки параметров в программе. 
+	 * Как только getopt() распознает параметр, конструкция switch 
+	 * определит, какой точно параметр был найден, и отметит 
+	 * соответствующее поле в структуре globalArgs. Когда getopt() 
+	 * вернет -1, то можно считать, что обработка параметров 
+	 * выполнена и оставшиеся аргументы являются файлами ввода.
+	 */
+	/* opt = getopt_long( argc, argv, optString, longOpts, &longIndex ) */
+	/* while( (opt = getopt( argc, argv, opt_string )) != -1 ) { */
+	while( (opt = getopt_long( argc, argv, opt_string, longOpts, &longIndex )
+) != -1 ) {
+		switch( opt ) {
+			case 'i':
+				global_args.line_increment = atoi(optarg);
+				break;
+			
+			case 's':
+				global_args.separator = optarg;	
+				break;
+				
+			case 'v':
+				global_args.start_number = atoi(optarg);
+				break;
+			
+			case 'w':
+				global_args.number_width = atoi(optarg);
+				break;
+				
+			case 'h':	/* переход к следующему кейсу */
+			case '?':
+				usage(argv[0]);
+				break;
+				
+			default:
+				/* если не один из кейсов не сработал */
+				break;
+		}
+	}
+	global_args.inputFiles    = argv + optind;
+	global_args.numInputFiles = argc - optind;
+}
+
+
+
+int main( int argc, char *argv[] )
+{
+
+	if (argc < 2)			/* если ключи не заданы */
+		usage(argv[0]);
+
+	validate_args(argc, argv);	/* проверка входных параметров */
+	
+	printf ("i = %d\n", global_args.line_increment);
+	printf ("s = %s\n", global_args.separator);
+	printf ("v = %d\n", global_args.start_number);
+	printf ("w = %d\n", global_args.number_width);
+	
+	printf ("files = %s\n", global_args.inputFiles[0]);
+	printf ("files = %s\n", global_args.inputFiles[1]);
+	printf ("files = %s\n", global_args.inputFiles[2]);
+	printf ("num_files = %d\n", global_args.numInputFiles);
+
+	return EXIT_SUCCESS;
+}
